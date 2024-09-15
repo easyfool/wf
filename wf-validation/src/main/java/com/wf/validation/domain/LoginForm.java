38ef3ef68e85c7dc8fd7ec2c37a6f557b2cf5c62
wangfeng <wangfengababe@163.com>
2024-09-15 21:22:29 +0800

    add: 自定义注解校验

--- wf-validation/src/main/java/com/wf/validation/domain/LoginForm.java
+++ wf-validation/src/main/java/com/wf/validation/domain/LoginForm.java
@@ -1,5 +1,6 @@
 package com.wf.validation.domain;
 
+import com.wf.validation.annotation.EnumString;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -22,4 +23,9 @@ public class LoginForm {
     @ApiModelProperty(value = "密码", required = true, example = "123456")
     @NotBlank(message = "[password] required!")
     private String password;
+
+
+    @ApiModelProperty(value = "性别")
+    @EnumString(value = {"F","M"}, message="性别只允许为F或M")
+    private String sex;
 }
