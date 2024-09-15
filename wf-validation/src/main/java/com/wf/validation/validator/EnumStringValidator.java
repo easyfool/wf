29d7fc80c0f21ae8b7c3df51af619287bef7322a
wangfeng <wangfengababe@163.com>
2024-09-15 21:22:29 +0800

    add: 自定义注解校验

---
+++ wf-validation/src/main/java/com/wf/validation/validator/EnumStringValidator.java
@@ -0,0 +1,31 @@
+package com.wf.validation.validator;
+
+import com.wf.validation.annotation.EnumString;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author wangfeng
+ * @version 1.0
+ * @mail wangfengbabe@163.com
+ * @data 2024/9/15 21:21
+ */
+public class EnumStringValidator implements ConstraintValidator<EnumString, String> {
+    private List<String> enumStringList;
+
+    @Override
+    public void initialize(EnumString constraintAnnotation) {
+        enumStringList = Arrays.asList(constraintAnnotation.value());
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if(value == null){
+            return true;
+        }
+        return enumStringList.contains(value);
+    }
+}
