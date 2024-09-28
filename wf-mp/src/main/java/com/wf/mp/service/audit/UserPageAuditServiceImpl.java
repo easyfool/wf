942b09b6aed6cae6bd72aa1d1de265ea9c280416
wangfeng <wangfengababe@163.com>
2024-09-28 17:22:11 +0800

    add: 审核公共实现

--- wf-mp/src/main/java/com/wf/mp/service/audit/UserPageAuditServiceImpl.java
+++ wf-mp/src/main/java/com/wf/mp/service/audit/UserPageAuditServiceImpl.java
@@ -1,16 +1,16 @@
-package com.wf.mp.service.audit;
-
-import com.wf.mp.domain.dto.UserDTO;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-
-/**
- * @author wangfeng
- * @version 1.0
- * @mail wangfengbabe@163.com
- * @data 2024/9/28 15:46
- */
-@Service
-@Slf4j
-public class UserPageAuditServiceImpl extends AuditServiceImpl<UserVO, UserDTO>{
-}
+//package com.wf.mp.service.audit;
+//
+//import com.wf.mp.domain.dto.UserDTO;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Service;
+//
+///**
+// * @author wangfeng
+// * @version 1.0
+// * @mail wangfengbabe@163.com
+// * @data 2024/9/28 15:46
+// */
+//@Service
+//@Slf4j
+//public class UserPageAuditServiceImpl extends AuditServiceImpl<UserVO, UserDTO>{
+//}
